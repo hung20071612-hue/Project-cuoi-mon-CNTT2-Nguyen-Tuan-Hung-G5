83a8332c59207d9770a11976d63df65839096510
hung20071612-hue <hung20071612@gmail.com>
2025-11-24 15:22:11 +0700

    Add files via upload

---
+++ bai_cuoi_mon.c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX 100
+struct Room {
+	char roomId[5];
+	int type;
+	double price;
+	int status; 
+};
+struct Room list[MAX] = {
+	{"101", 1, 350000, 0},
+	{"234", 2, 400000, 0},
+	{"132", 1, 200000, 0}
+};
+int n = 3;
+struct Booking {
+	char bookId[20];
+	char roomId[5];
+	char customerName[50];
+	int days;
+	double totalCost;
+};
+void display();
+void add_room();
+void update_room();
+int main(){
+	int choice;
+	do{
+		display();
+		printf("moi ban nhap lua chon: ");
+		scanf("%d",&choice);
+		getchar();
+		switch(choice){
+			case 1:
+				add_room();
+				break;
+			case 2:
+				update_room();
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			default:
+					printf("moi ban nhap lai!!!!\n");
+				break;
+		}
+	}while(1);
+	return 0;
+}
+void display(){
+	printf("+-----------------------------Mini Hotel-----------------------------+\n");
+	printf("|1. Them phong moi                                                   |\n");
+	printf("|2. Cap nhat phong                                                   |\n");
+	printf("|3. Bao tri phong (khoa)                                             |\n");
+	printf("|4. Hien thi danh sach                                               |\n");
+	printf("|5. Tim kiem                                                         |\n");
+	printf("|6. Sap xep danh sach phong (theo gia phong)                         |\n");
+	printf("|7. Check-in (Giao dich)                                             |\n");
+	printf("|8. Lich su thue                                                     |\n");
+	printf("+--------------------------------------------------------------------+\n");
+}
+void add_room(){
+	int flag = 1;
+	do{
+		flag = 1;
+		printf("moi ban nhap so phong: ");
+		fgets(list[n].roomId,sizeof(list[n].roomId),stdin);
+		list[n].roomId[strcspn(list[n].roomId, "\n")] = 0;
+		if(strlen(list[n].roomId) == 0){
+			flag = 0;
+			printf("Loi: so phong khong duoc de trong!\n");
+			continue;
+		}
+		for(int i = 0; i < n ; i++){
+			if(strcmp(list[n].roomId, list[i].roomId) == 0){
+				flag = 0;
+				printf("Loi: Phong %s da ton tai!\n",list[n].roomId);
+				break;
+			}
+		}
+	}while(flag != 1);
+	do{
+		printf("moi ban nhap loai phong (1/2): ");
+		scanf("%d",&list[n].type);
+		getchar();
+		if(list[n].type != 1 && list[n].type != 2){
+			printf("Loi: loai phong chi duoc chon 1(Don) hoac 2(Doi)!\n");
+		}
+	}while(list[n].type != 1 && list[n].type != 2);
+	do{
+		printf("moi ban nhap gia phong %s: ",list[n].roomId);
+		scanf("%lf",&list[n].price);
+		getchar();
+		if(list[n].price <= 0){
+			printf("Loi: Gia phong phai lon hon 0!\n");
+			continue;
+		}
+	}while(list[n].price <= 0);
+	list[n].status = 0;
+	printf("Them phong thanh cong! Phong %s da duoc tao voi trang thai trong.\n",list[n].roomId);
+	n++;
+}
+void update_room(){
+	int flag = 0;
+	char find_Id[10];
+	printf("moi ban nhap so phong can cap nhat: ");
+	fgets(find_Id, sizeof(find_Id), stdin);
+	find_Id[strcspn(find_Id, "\n")] = 0;
+	for(int i = 0; i < n; i++){
+		if(strcmp(find_Id,list[i].roomId) == 0){
+			flag = 1;
+		do{
+		printf("moi ban nhap loai phong (1/2): ");
+		scanf("%d",&list[i].type);
+		getchar();
+		if(list[i].type != 1 && list[i].type != 2){
+			printf("Loi: loai phong chi duoc chon 1 hoac 2!\n");
+			}
+		}while(list[i].type != 1 && list[i].type != 2);
+		do{
+		printf("moi ban nhap gia phong %s: ",list[i].roomId);
+		scanf("%lf",&list[i].price);
+		getchar();
+		if(list[i].price <= 0){
+			printf("Loi: Gia phong phai lon hon 0!\n");
+			continue;
+			}
+		}while(list[i].price <= 0);
+		printf("Cap nhat thong tin phong %s thanh cong!\n", list[i].roomId);
+			}
+		}
+	if(flag == 0){
+		printf("Loi: khong tim thay phong %s!\n",find_Id);
+		}
+}
